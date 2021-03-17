b7b9d0023b710177810771f8086c7f50eca847ba
Tino - Milton <miltonjacomini@gmail.com>
2021-03-16 21:21:19 -0300

    Adicionando estrutura do projeto

---
+++ src/test/java/br/com/coletivogaia/NativeSnsConsumerAndS3WrapperIT.java
@@ -0,0 +1,9 @@
+package br.com.coletivogaia;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeSnsConsumerAndS3WrapperIT extends SnsConsumerAndS3WrapperTest {
+
+    // Execute the same tests but in native mode.
+}
